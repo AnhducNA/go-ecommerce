f50f4a17c9fac79875161797a8f6afd9a58f77a4
leanhduc <ducank9612gmail.com>
2025-04-09 07:25:59 +0700

    wire dependency injection

---
+++ internal/wire/user.wire.go
@@ -0,0 +1,18 @@
+// go:build-wireinject
+package wire
+
+import (
+	"github.com/AnhducNA/go-ecommerce/internal/controller"
+	"github.com/AnhducNA/go-ecommerce/internal/repo"
+	"github.com/AnhducNA/go-ecommerce/internal/service"
+	"github.com/google/wire"
+)
+
+func InitUserRouterHandler() (*controller.UserController, error) {
+	wire.Build(
+		repo.NewUserRepository,
+		service.NewUserServie,
+		controller.NewUserController,
+	)
+	return new(controller.UserController), nil
+}
